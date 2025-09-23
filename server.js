2ce0476d79757662f2988ae6266cab30a6c9ab2b
Sora3-code <jump22431129@gmail.com>
2025-09-23 20:35:06 +0900

    modified the code 9/23 20:34.

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(express.static('public'));
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`server start ${PORT} (*^^)v`);
