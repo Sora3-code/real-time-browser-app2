a8783f605071381025c3ce777813ab4a0830d441
Sora3-code <jump22431129@gmail.com>
2025-09-23 17:18:54 +0900

    modified the code 9/23 17:18.

---
+++ server.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`server start ${PORT} (*^^)v`);
+});
